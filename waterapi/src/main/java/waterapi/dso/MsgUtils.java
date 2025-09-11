b74df92da51a3f414aaae7c499b2647fce55a8c6
noear <noear@live.cn>
2025-09-11 12:04:50 +0800

    2.17.0

--- waterapi/src/main/java/waterapi/dso/MsgUtils.java
+++ waterapi/src/main/java/waterapi/dso/MsgUtils.java
@@ -1,3 +1,12 @@
+/*
+ *  Copyright 2017-2025 noear.org
+ *
+ *  企业用户未经 noear 组织特别许可，需遵循 AGPL-3.0 开源协议合理合法使用本项目。
+ *
+ *   Enterprise users are required to use this project reasonably
+ *   and legally in accordance with the AGPL-3.0 open source agreement
+ *  without special permission from the smartboot organization.
+ */
 package waterapi.dso;
 
 import org.noear.water.WW;
